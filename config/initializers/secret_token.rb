00931df16e9a35aae2a7d1c63a6e77d5625534fb
grandbora <bora.tunca@yahoo.com>
2012-11-25 17:11:17 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Repo::Application.config.secret_token = 'c0ae239089c8382597ea4f7c12b05de82c90d6fca4a3d4fd8b86563255e6d289fb073de2e65d05cab72d6c0ab3e3dabfad38fb3d033977130fd05d879889ae9c'
